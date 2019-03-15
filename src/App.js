70bc55fd26fd1adff31b7951f9affa8d03bf2f0d
Vaibhav Chhabra <vaibhav.chhabra@imrggn.com>
2019-03-15 16:47:38 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router,Route,Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.components";
 
 class App extends Component {
   render() {
     return (
         <Router>
-          <div className="container">
-            <h1>THIS IS  A Gyu</h1>
+          <div>
+                  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                      <a className="navbar-brand" href="https://www.google.com" target="_blank">
+                          <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+                      </a>
+                      <Link to="/" className="navbar-brand">Todo App</Link>
+                      <div className="collpase nav-collapse">
+                          <ul className="navbar-nav mr-auto">
+                              <li className="navbar-item">
+                                  <Link to="/" className="nav-link">Todos</Link>
+                              </li>
+                              <li className="navbar-item">
+                                  <Link to="/create" className="nav-link">Create Todo</Link>
+                              </li>
+                          </ul>
+                      </div>
+                  </nav>
+              <div className="container">
+              </div>
+            <Route path="/" exact component={TodosList}/>
+            <Route path="/edit/:id" exact component={EditTodo}/>
+            <Route path="/create" exact component={CreateTodo}/>
           </div>
-
-            <Route path="/" exact component={TodoList}/>
 
         </Router>
     );
--- src/components/todos-list.components.js
+++ src/components/todos-list.components.js
@@ -1,1 +1,11 @@
-import
+import React,{Component} from 'react';
+
+export default class TodosList extends Component{
+    render(){
+        return (
+            <div>
+                <p>Welcome to T0DO list</p>
+            </div>
+        )
+    }
+}
